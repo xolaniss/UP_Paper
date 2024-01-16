f3e60bbb789b5398edc633bfae0ae617efe93ff8
Xolani Sibande <xolaniss@gmail.com>
2024-01-16 12:26:10 +0200

    updated descriptive table with narrative indices

--- Scripts/07_descriptives.R
+++ Scripts/07_descriptives.R
@@ -38,6 +38,21 @@ library(car)
 source(here("Functions", "fx_plot.R"))
 
 # Import -------------------------------------------------------------
+macropru_index <- read_rds(here("Outputs", 
+                                "narratives", 
+                                "artifacts_macropru_narratives.rds"))
+macropru_index_tbl <- macropru_index$macropru_narratives_tbl %>% 
+  pivot_longer(cols = -Date, names_to = "Series", values_to = "Value")
+macropru_index_tbl
+
+competition_index <- read_rds(here("Outputs", 
+                                   "narratives", 
+                                   "artifacts_competion_narratives.rds"))
+competition_index_tbl <- competition_index$data$combined_dummies_tbl %>% 
+  dplyr::select(-c(Event, Description)) %>%
+  pivot_longer(cols = -Date, names_to = "Series", values_to = "Value")
+competition_index_tbl
+
 lending <- read_rds(here("Outputs", "BA900", "artifacts_BA900.rds"))
 aggregated_lending_tbl <- lending$aggregated_data$combined_aggregated_lending_tbl
 aggregated_lending_tbl 
@@ -55,59 +70,55 @@ controls_tbl <-
 controls_tbl
 
 # Descriptives --------------------------------------------------------
+banks_descriptives <- function (data, group_var){
+  data %>% 
+    drop_na() %>%
+    group_by(Series) %>%
+    summarise(across(
+      .cols = -c(Date),
+      .fns = list(
+        Median = median,
+        SD = sd,
+        Min = min,
+        Max = max,
+        IQR = IQR,
+        Obs = ~ n()
+      ),
+      .names = "{.fn}"
+    )) %>%
+    mutate(Group = group_var)
+  }
+
 descriptives_lending_tbl <- 
   aggregated_lending_tbl %>% 
-  filter(Banks == "Total Banks") %>%
-  dplyr::select(-Banks) %>% 
-  group_by(Series) %>% 
-  summarise(across(.cols = -c(Date),
-                   .fns = list(Median = median, 
-                               SD = sd,
-                               Min = min,
-                               Max = max,
-                               IQR = IQR,
-                               Obs = ~ n()), 
-                   .names = "{.fn}")) %>% 
-  mutate(Group = "Lending")
-
-
+  filter(Banks == "Total Banks") %>% 
+  dplyr::select(-Banks) %>%
+  banks_descriptives(group_var = "Lending")
+  
 descriptives_lending_rates_tbl <- 
   aggregated_lending_rates_tbl %>% 
   filter(Banks == "Total Banks") %>% 
-  dplyr::select(-Banks) %>% 
-  group_by(Series) %>% 
-  summarise(across(.cols = -c(Date),
-                   .fns = list(Median = median, 
-                               SD = sd,
-                               Min = min,
-                               Max = max,
-                               IQR = IQR,
-                               Obs = ~ n()), 
-                   .names = "{.fn}")) %>% 
-  mutate(Group = "Lending Rates")
-
+  dplyr::select(-Banks) %>%
+  banks_descriptives(group_var = "Lending Rates")
+  
 descriptives_control_tbl <- 
   controls_tbl %>%
-  drop_na() %>%
+  filter(Banks == "Total Banks") %>% 
   dplyr::select(-Banks) %>%
-  group_by(Series) %>%
-  summarise(across(
-    .cols = -c(Date),
-    .fns = list(
-      Median = median,
-      SD = sd,
-      Min = min,
-      Max = max,
-      IQR = IQR,
-      Obs = ~ n()
-    ),
-    .names = "{.fn}"
-  )) %>%
-  mutate(Group = "Controls")
+  banks_descriptives(group_var = "Controls")
 
+descriptives_macropru_tbl <-
+  macropru_index_tbl %>% 
+  banks_descriptives(group_var = "Macroprudential Narrative Indices")
+
+competition_index_tbl <-
+  competition_index_tbl %>% 
+  banks_descriptives(group_var = "Competition Narrative Indices")
 
 descriptives_tbl <- 
   bind_rows(
+    descriptives_macropru_tbl,
+    competition_index_tbl,
     descriptives_lending_tbl,
     descriptives_lending_rates_tbl,
     descriptives_control_tbl
@@ -118,7 +129,9 @@ artifacts_descriptives <- list (
   data = list(
     aggregated_lending_tbl = aggregated_lending_tbl,
     aggregated_lending_rates_tbl = aggregated_lending_rates_tbl,
-    controls_tbl = controls_tbl
+    controls_tbl = controls_tbl,
+    macropru_index_tbl = macropru_index_tbl,
+    competition_index_tbl = competition_index_tbl
   ),
   desc = list(
     descriptives_tbl = descriptives_tbl
