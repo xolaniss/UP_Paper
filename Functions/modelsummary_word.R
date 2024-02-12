2bc3b92f6c40563fb2c3d6594332cd8ecd63c017
Xolani Sibande <xolaniss@gmail.com>
2024-02-12 15:42:10 +0200

    updated macropru tables

--- Functions/modelsummary_word.R
+++ Functions/modelsummary_word.R
@@ -2,26 +2,31 @@ modelsummary_word <- function(models = models,
                               title = "Insert title", 
                               coef_map = NULL, 
                               coef_rename  = NULL,
-                              stars = TRUE, 
+                              stars = c('*' = 0.1, '**' = 0.05, '***' = 0.01), 
                               decimals = 2,
                               vcov =  NULL,
-                              variables_omit = "AIC|BIC|Log|RMSE",
-                              coef_omit = NULL
+                              gof_omit = "AIC|BIC|Std|Log",
+                              coef_omit = NULL,
+                              add_rows = NULL,
+                              gof_map =  gof_map,
+                              output = "flextable"
 ) {
   modelsummary(
     models,
-    output = "flextable",
+    output = output,
+    statistic = NULL,
     stars = stars,
     fmt = decimals,
     vcov =  vcov,
     title = title,
-    gof_omit = variables_omit ,
+    gof_omit = gof_omit ,
     coef_map = coef_map,
     coef_rename = coef_rename,
     coef_omit = coef_omit,
-    gof_map = "all"
+    gof_map = "all",
+    add_rows = add_rows
     
-  ) %>%
-    theme_booktabs() %>%
-    border_inner(border = fp_border(color = "white"), part = "all") 
+  ) 
+    # theme_booktabs() 
+    # border_inner(border = fp_border(color = "white"), part = "all")
 }
